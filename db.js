d25e8d72ce8731e9a9e81af94b63bbe25c337cac
Bangsi <bangsir61@gmail.com>
2021-08-22 18:03:44 +0100

    Update in database details

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const uri = process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb://localhost:27017/UserDatabase";
 mongoose.connect(
-  "mongodb://localhost:27017/UserDatabase",
+  url,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
