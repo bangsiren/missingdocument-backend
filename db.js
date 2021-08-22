12fded19dbcf7012a5174c09fcdbc334b6917eb3
Bangsi <bangsir61@gmail.com>
2021-08-22 18:06:47 +0100

    typo fix

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const uri = process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb://localhost:27017/UserDatabase";
 mongoose.connect(
-  url,
+  uri,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
