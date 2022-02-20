d3b52ec3e263f176d56c7abfe74d1f6338f4694f
mistazidane <ntokungwiazidane@yahoo.com>
2022-02-20 15:58:48 +0100

    ok

--- controller/fileUpload.js
+++ controller/fileUpload.js
@@ -1,5 +1,7 @@
+const { Buffer } = require('buffer');
 var cloudinary = require('cloudinary');
 const formidable = require('formidable')
+const axios = require('axios');
 cloudinary.config({
     cloud_name: 'b-tec',
     api_key: '544984634748171',
@@ -18,19 +20,59 @@ exports.uploadFile = (req, res) => {
     const form = new formidable.IncomingForm()
     // res.send('Sending')
     form.on('file', (field, file) => {
+        // console.log(file);
         // Do something with the file
         // e.g. save it to the database
         // you can access it using file.path
-        fileUploader(file.path).then(val => {
+        // convert file to base64 to use in model
+        var fs = require('fs');
+
+// function to encode file data to base64 encoded string
+// function base64_encode(path) {
+//     // read binary data
+//     var bitmap = fs.readFileSync(file);
+//     // convert binary data to base64 encoded string
+//     return new Buffer(bitmap).toString('base64');
+// }
+// console.log(base64_encode(file.path+""));
+var bitmap = fs.readFileSync(file.path);
+let ss = bitmap.toString('base64');
+
+
+const data = {
+    image: ss,
+};
+
+axios.post('http://127.0.0.1:5000/predict', data)
+    .then((response) => {
+  let sorted = response.data.predictions.sort((a,b)=>{
+            return a.confidence - b.confidence;
+        });
+        if(sorted[0].label == "Others"){
+            console.log("correct document");
+                   fileUploader(file.path).then(val => {
             console.log('Runningg')
             res.json({
                 success: true,
                 name: req.body.name,
+                message:"Correct Document",
                 path: val.secure_url
             });
         }).catch(er => {
             res.json({ success: false })
         });
+        }
+        else{
+            res.json({ success: false , message:"wrong document"})
+        }
+
+    }).catch((err) => {
+        console.error(err);
+        res.json({ success: false })
+    });
+     
+
+
     });
 
     // exports.deletingFile = (req, res) =>{
